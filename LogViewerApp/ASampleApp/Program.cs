690829a68f485c4d226f8695800f4cbf25e224f2
Augustin Calin <augustincalin@yahoo.com>
2018-01-30 00:00:38 +0100

    adds ELK and an extra debian for a simple .NET Core console log generator using log4net

---
+++ LogViewerApp/ASampleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace ASampleApp {
+    class Program {
+        static void Main(string[] args) {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+            var logger = LogManager.GetLogger(typeof(Program));
+            logger.Info("Program started!");
+            string message = string.Empty;
+            bool goOn = true;
+            Console.WriteLine("LOG GENERATOR");
+            Console.WriteLine("=============");
+
+            while (goOn) {
+                Console.Write("Warn, Error, Info, Quit ( W / E / I / Q ): ");
+                ConsoleKeyInfo key = Console.ReadKey();
+                goOn = key.Key != ConsoleKey.Q;
+                Console.WriteLine();
+                if (goOn) {
+                    Console.Write("Message: ");
+                    message = Console.ReadLine();
+                }
+                switch (key.Key) {
+                    case ConsoleKey.W:
+                        logger.Warn(message);
+                        break;
+                    case ConsoleKey.E:
+                        logger.Error(message);
+                        break;
+                    case ConsoleKey.I:
+                        logger.Info(message);
+                        break;
+                }
+            };
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,13 +2,22 @@
 ## Install
 First you install docker. Then istall docker-compose. Then clone this.
 ## Docker
-Run `docker-compose up`. This will launch 3 containers: Elasticsearch, Logstash, Kibana (see docker-compose.yml):
+### ASampleApp
+Build the image in ASampleApp: `docker build -t asampleapp .`. This will create an image named asampleapp. In order to connect to the container, execute `docker exec -it aspnet bash` (after you launch it via `docker-compose up`), then launch the application: `dotnet ASampleApp.dll`. Start generating entries in the log.
+### Docker compose
+Run `docker-compose up`. This will launch XXX containers: Elasticsearch, Logstash, Kibana, asampleapp (see docker-compose.yml):
 | What                    | Where | How  |
 |-------------------------|:------|------|
 | Elasticsearch (input)   | 5000  | TCP  |
 | Elasticsearch (http)    | 9200  | HTTP |
 | Logstash (input)        | 5000  | TCP  |
 | Kibana                  | 5601  | HTTP |
+| asampleapp              | -     | -    |
+
+Then you have to feed elasticsearch via logstash: `echo "abc" | nc localhost 5000`. 
+
+After that you can use kibana to define an index and see the data: http://localhost:5601.
+
 
 ### Still to do:
 - [ ] complete the environment by launching other containers: web app, sql server?, some x server...
